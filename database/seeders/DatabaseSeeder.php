162d23c686703c35e4eabc7963d49140287863ed
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-03 16:26:21 +0300

    Run test accounts seeder with database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,7 +23,7 @@ class DatabaseSeeder extends Seeder
             CoursesTableSeeder::class,
             LessonsTableSeeder::class,
             UsersTableSeeder::class,
-
+            TestAccountsSeeder::class,
         ]);
     }
 }
