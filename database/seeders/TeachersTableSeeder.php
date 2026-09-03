9f1ac9a17a46a8f47240afbe1f00d7f1e0683255
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-03 16:27:09 +0300

    Make teacher seeder idempotent for repeatable database seeding

--- database/seeders/TeachersTableSeeder.php
+++ database/seeders/TeachersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Enums\GenderTypesEnums;
 use App\Models\Teacher;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class TeachersTableSeeder extends Seeder
 {
@@ -13,16 +14,20 @@ class TeachersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Teacher::create([
-            "name" => 'test',
-            "email_verified_at"=>now(),
-            "email"=>"taest@gmail.com",
-            "phone"=>"0123456789",
-            "password"=>"password",
-            "address"=>"test address",
-            "description"=>"test description",
-            "education_level_id"=>1,
-            "gender"=>GenderTypesEnums::Male,
-        ]);
+        Teacher::query()->updateOrCreate(
+            [
+                'email' => 'taest@gmail.com',
+            ],
+            [
+                'name' => 'test',
+                'email_verified_at' => now(),
+                'phone' => '0123456789',
+                'password' => Hash::make('password'),
+                'address' => 'test address',
+                'description' => 'test description',
+                'education_level_id' => 1,
+                'gender' => GenderTypesEnums::Male,
+            ]
+        );
     }
 }
